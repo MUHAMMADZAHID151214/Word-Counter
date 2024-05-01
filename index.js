3edef48f01ab97389a0cbff4eeefaa56362995db
Muhammad Zahid <164073545+MUHAMMADZAHID151214@users.noreply.github.com>
2024-05-01 23:42:57 +0500

    Word Conter Project Code

---
+++ index.js
@@ -0,0 +1,10 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence : "
+    }]);
+const words = answer.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+const answer:{
+    Sentence:string
+} = await inquirer.prompt([{
+    name:"Sentence",
+    type:"input",
+    message:"Enter your sentence : "
+
+
+}])
+
+const words = answer.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
